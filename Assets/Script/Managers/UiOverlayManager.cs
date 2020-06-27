7daaa6eb00710ad3d1c763510b79f0bdd2252678
Kyzure <Kyzurene@gmail.com>
2020-06-27 12:55:35 +0800

    Added stuff mainmenu

---
+++ Assets/Script/Managers/UiOverlayManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UiOverlayManager : Manager<UiOverlayManager> {    
+
+    new void Awake() {
+        base.Awake();
+        SceneManager.LoadScene("UiOverlay", LoadSceneMode.Additive);
+    }
+}
